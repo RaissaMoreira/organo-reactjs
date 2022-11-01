8faba2309f2f415bf512cb3ac5a48d724398a068
RaissaMoreira <raissamoreira80@gmail.com>
2022-11-01 18:10:11 -0300

    created component team + raised list of teams

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/team';
 
 function App() {
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Front End',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#db6ebf',
+      corSecundaria: '#fae9f5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#ffba05',
+      corSecundaria: '#fff5d9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#ff8a29',
+      corSecundaria: '#ffeedf'
+    }
+  ];
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -13,7 +52,11 @@ function App() {
   return (
     <div className="App">
       <Banner/>
-      <Form aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Form 
+        times={times.map(time => time.nome)}
+        aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}
+      />
+      {times.map(time => <Team key={time.nome} name={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
     </div>
   );
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,16 +5,6 @@ import InputField from "../InputField";
 import "./Form.css";
 
 const Form = (props) => {
-  const times = [
-    "Programação",
-    "Front-End",
-    "Data Science",
-    "Devops",
-    "UX e Design",
-    "Mobile",
-    "Inovação e Gestão"
-  ];
-
   const [nome, setNome] = useState('');
   const [cargo, setCargo] = useState('');
   const [imagem, setImagem] = useState('');
@@ -57,7 +47,7 @@ const Form = (props) => {
         <DropDownList
           obrigatorio={true}
           label="Time"
-          itens={times}
+          itens={props.times}
           valor={time}
           aoAlterado={valor => setTime(valor)}
         />
